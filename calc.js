724d9df76299adbf738a6b9baf92d05ffa8f9402
Aleksander Okrasa <olekokrasa@gmail.com>
2023-02-04 15:23:18 +0100

    Create subtraction, multiplication and division functions for the BasicCalculator class; Create BasicInputTaker function and in it 2 methods in prototypes that handle the object of BasicCalculator class

--- calc.js
+++ calc.js
@@ -5,26 +5,77 @@ class BasicCalculator{
         this.y = y;
     }
 
-    add() {
+    add(){
         this.result = this.x + this.y;
-        console.log(`Result of ${this.x} + ${this.y} = ${this.result}`)
+        console.log(`Result: ${this.x} + ${this.y} = ${this.result}`)
+        document.write(`Result: ${this.x} + ${this.y} = ${this.result}`)
+    }
+
+    subtract(){
+        this.result = this.x - this.y;
+        console.log(`Result: ${this.x} - ${this.y} = ${this.result}`)
+        document.write(`Result: ${this.x} - ${this.y} = ${this.result}`)
+    }
+
+    multiply(){
+        this.result = this.x * this.y;
+        console.log(`Result: ${this.x} * ${this.y} = ${this.result}`)
+        document.write(`Result: ${this.x} * ${this.y} = ${this.result}`)
+    }
+
+    divide(){
+        this.result = this.x / this.y;
+        console.log(`Result: ${this.x} / ${this.y} = ${this.result}`)
+        document.write(`Result: ${this.x} / ${this.y} = ${this.result}`)
     }
 
 };
 
 
 
-let input_x = prompt("Give x: ")
-let int_x = parseInt(input_x)
-let input_y = prompt("Give y: ")
-let int_y = parseInt(input_y)
-console.log(int_x);
-console.log(int_y);
-
-const calc = new BasicCalculator(int_x, int_y);
-
-let input_action = prompt("What do you want to do?")
-
-if (input_action ="add"){
-    calc.add()
+function BasicInputTaker(){
+/*
+I have read that it is more recommended to use classes when inheriting, 
+but for the purpose of learning the possibilities of inheritance and object-orientedness in JS,
+I used the function here
+*/
+    this.input_x = prompt("Give x: ")
+    this.input_y = prompt("Give y: ")
 }
+
+BasicInputTaker.prototype.runBasicCalculator = function() {
+    this.int_x = parseInt(this.input_x)
+    this.int_y = parseInt(this.input_y)
+    console.log(this.int_x);
+    console.log(this.int_y);
+    this.calc = new BasicCalculator(this.int_x, this.int_y);
+}
+
+BasicInputTaker.prototype.selectActionBasicCalculator = function() {
+
+    this.input_action = prompt("What do you want to do?")
+
+    switch (this.input_action) {
+
+        case '+':
+            this.calc.add();
+            break;
+        case '-':
+            this.calc.subtract();
+            break;
+        case '*':
+            this.calc.multiply();
+            break;
+        case '/':
+            this.calc.divide();
+            break;
+
+        default:
+            console.log(`Bad input action value, you sent "${input_action}", try: '+', '-', '*', '/'.`);
+            document.write(`Bad input action value, you sent "${input_action}", try: '+', '-', '*', '/'.`);
+    };
+};
+
+const input = new BasicInputTaker();
+input.runBasicCalculator();
+input.selectActionBasicCalculator();
