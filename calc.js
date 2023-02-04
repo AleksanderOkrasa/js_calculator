bce1f1eef3aa9f0bfdf513c53c7c81f36575f39b
Aleksander Okrasa <olekokrasa@gmail.com>
2023-02-04 15:56:22 +0100

    Create "ExtendedInputTaker" function inheriting from "BasicInputTaker" function constructor, prototype and override "runBasicCalculator" method

--- calc.js
+++ calc.js
@@ -40,14 +40,16 @@ but for the purpose of learning the possibilities of inheritance and object-orie
 I used the function here
 */
     this.input_x = prompt("Give x: ")
+    console.log("input x: " + this.input_x);
     this.input_y = prompt("Give y: ")
+    console.log("input y: " + this.input_y);
 }
 
 BasicInputTaker.prototype.runBasicCalculator = function() {
     this.int_x = parseInt(this.input_x)
     this.int_y = parseInt(this.input_y)
-    console.log(this.int_x);
-    console.log(this.int_y);
+    console.log("int x: " + this.int_x);
+    console.log("int y: " + this.int_y);
     this.calc = new BasicCalculator(this.int_x, this.int_y);
 }
 
@@ -76,6 +78,25 @@ BasicInputTaker.prototype.selectActionBasicCalculator = function() {
     };
 };
 
-const input = new BasicInputTaker();
+function ExtendedInputTaker(){
+    BasicInputTaker.call(this);
+    // Inheritance of the base function constructor
+}
+
+ExtendedInputTaker.prototype = Object.create(BasicInputTaker.prototype);
+// Inheritance of a prototype from a base function
+
+ExtendedInputTaker.prototype.runBasicCalculator = function() {
+// Overriding a method from the base function ( change from int to float )
+    this.float_x = parseFloat(this.input_x)
+    this.float_y = parseFloat(this.input_y)
+    console.log("int x: " + this.float_x);
+    console.log("int y: " + this.float_y);
+    this.calc = new BasicCalculator(this.float_x, this.float_y);
+}
+
+
+// const input = new BasicInputTaker();
+const input = new ExtendedInputTaker();
 input.runBasicCalculator();
 input.selectActionBasicCalculator();
